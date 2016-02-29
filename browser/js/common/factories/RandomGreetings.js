988af9b94f1cb707b679f4461f1e4c67ce8bb590
MrNuggetPhd <jalves443@gmail.com>
2016-02-29 16:12:26 -0500

    Update RandomGreetings.js

--- browser/js/common/factories/RandomGreetings.js
+++ browser/js/common/factories/RandomGreetings.js
@@ -15,7 +15,7 @@ app.factory('RandomGreetings', function () {
         'Hello, 1601!',
         'こんにちは、ユーザー様。',
         'Welcome. To. WEBSITE.',
-        ':D',
+        ':(',
         'Yes, I think we\'ve met before.',
         'Gimme 3 mins... I just grabbed this really dope frittata',
         'If Cooper could offer only one piece of advice, it would be to nevSQUIRREL!',
